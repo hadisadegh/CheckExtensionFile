954bcde67b747942540d8ecfd964affab6baf663
hadi <hadisdegh@yahoo.com>
2023-01-15 13:48:44 +0330

    first commit

---
+++ CheckIfImageFile.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Baselayer.Filters
+{
+    public static class CheckExtensionFile
+    {
+        public static bool ImageFile(IFormFile file)
+        {
+            var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
+            return (extension == ".jpg" || extension == ".png" || extension == ".jpeg" || extension == ".svg"); // Change the extension based on your need
+        }
+
+        public static bool Size(IFormFile file, int LengthfilterByte)
+        {
+            if (file.Length > LengthfilterByte)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
